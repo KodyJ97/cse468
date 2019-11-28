7cdbdb9797c45c5d679b7f0e7576001a4fb4d46d
Kody Johnson <kkjohns9@asu.edu>
2019-11-28 10:50:20 -0700

    Initial File

---
+++ matrix.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main()
+{
+	printf("matrix.c");
+	
+}
+
+
+/*
+void* mygemm(void* arg)
+{
+	// Matrix Multiplication
+	for(int i = 0; i < N; i++)
+		for(int j = 0; j < N; j++)
+			for(int k = 0; k < N; k++)
+				matC[i][j] += matA[i][k] * matB[k][j];
+}
+*/
