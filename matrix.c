d8ef7673d76510c4c93eaeb998a380ef2c3cc11f
Kody Johnson <kkjohns9@asu.edu>
2019-12-04 16:06:59 -0700

    Update to have clock for milliseconds not sure if it is working right would be nice to have someone to comapre with.

--- matrix.c
+++ matrix.c
@@ -1,13 +1,16 @@
 #include <stdio.h> 
 #include <stdlib.h> 
+#include <pthread.h>
+#include <sys/time.h>
 
 // Matrix column and row value. Possible sizes: 128, 256, 512, and 1024.
 #define SIZE 128
+#define THREAD_COUNT 2
 
 // MAX_NUMBER and MIN_NUMBER are specifying the range of the random numbers
 // generated for matrix A and matrix B.
 #define MIN_NUMBER 1
-#define MAX_NUMBER 10
+#define MAX_NUMBER 5
 
 void func(int mat[SIZE][SIZE])
 {
@@ -17,9 +20,10 @@ void func(int mat[SIZE][SIZE])
 			mat[i][j] = (rand() % (MAX_NUMBER + 1 - MIN_NUMBER) + MIN_NUMBER);
 }
 
-int mygemm(int matA[SIZE][SIZE], int matB[SIZE][SIZE], int matC[SIZE][SIZE])
+void mygemm(int matA[SIZE][SIZE], int matB[SIZE][SIZE], int matC[SIZE][SIZE])
 {
 	// Matrix Multiplication
+	int count = 0;
 	for(int i = 0; i < SIZE; i++)
 		for(int j = 0; j < SIZE; j++)
 			for(int k = 0; k < SIZE; k++)
@@ -37,13 +41,18 @@ void print_Matrix(int mat[SIZE][SIZE])
 
 int main()
 {
+	struct timeval start, stop;
 	printf("matrix.c\n");
 	int matA[SIZE][SIZE];
 	int matB[SIZE][SIZE];
 	int matC[SIZE][SIZE];
 	func(matA);
 	func(matB);
+	gettimeofday(&start, NULL);
 	mygemm(matA, matB, matC);
-	print_Matrix(matC);
+	gettimeofday(&stop, NULL);
+	//print_Matrix(matC);
+	// Execution time
+	printf("Time of execution in milliseconds: %lu\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
 	return 0;
 }
