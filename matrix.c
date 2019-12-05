b32da15b1dd7027eee33b51491585fb07bcb3480
agcaddy74 <44042146+agcaddy74@users.noreply.github.com>
2019-12-05 11:40:28 -0700

    Threads are working with console print outs
    Still need to figure out a way to divvy up Matrix A into the required number of threads, right now it is just going through the whole of matrix A twice. Haven't tested larger matrix sizes or more threads yet since we need to fix this first.

--- matrix.c
+++ matrix.c
@@ -1,7 +1,7 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <pthread.h>
-#include <sys/time.h>
+#include <time.h>
 #include <unistd.h>  
 
 // Matrix column and row value. Possible sizes: 128, 256, 512, and 1024.
@@ -18,33 +18,41 @@ int matA[SIZE][SIZE];
 int matB[SIZE][SIZE];
 int matC[SIZE][SIZE];
 int step_test = 0;
-	
+
 void print_Matrix(int mat[SIZE][SIZE])
 {
-	int i,j;
-	for(i = 0; i < SIZE; i++)
-		for(j = 0; j < SIZE; j++)
-			printf("%d\n", mat[i][j]);
+	int i, j, rowcount = 0;
+	printf("Printing Matrix\n");
+	for (i = 0; i < SIZE; i++) {
+		for (j = 0; j < SIZE; j++) {
+			printf("Index [%d][%d]: %d\t", i, j, mat[i][j]);
+			rowcount++;
+			if (rowcount == 5) {
+				printf("\n");
+				rowcount = 0;
+			}
+		}
+	}
 }
 
 void func(int mat[SIZE][SIZE])
 {
-	int i,j;
-	for(i = 0; i < SIZE; i++)
-		for(j = 0; j < SIZE; j++)
+	int i, j;
+	for (i = 0; i < SIZE; i++)
+		for (j = 0; j < SIZE; j++)
 			mat[i][j] = (rand() % (MAX_NUMBER + 1 - MIN_NUMBER) + MIN_NUMBER);
 }
 
-void *mygemm(void *vargp)
+void* mygemm(void* vargp)
 {
 	// Geeks for geeks does a weird thing here with steps i dont think
 	// we do it this way
-	int test = step_test;
-	// Matrix Multiplication
-	for(int i = 0; i < SIZE; i++)
-		for(int j = 0; j < SIZE; j++)
-			for(int k = 0; k < SIZE; k++)
+	printf("Thread %ld: Starting mygemm\n", pthread_self());
+	for (int i = 0; i < SIZE; i++)
+		for (int j = 0; j < SIZE; j++)
+			for (int k = 0; k < SIZE; k++)
 				matC[i][j] += matA[i][k] * matB[k][j];
+	printf("Thread %ld: Ending mygemm and terminating threads\n", pthread_self());
 	// Terminate thread here?
 	pthread_exit(NULL);
 }
@@ -52,34 +60,36 @@ void *mygemm(void *vargp)
 int main()
 {
 	// Instantiate some time variables
-	struct timeval start, stop;
-	
+	clock_t startClock, endClock;
+
 	// Randomize the matrixes A and B
 	func(matA);
 	func(matB);
-	
+
 	// Start time
-	gettimeofday(&start, NULL);
-	
+	startClock = clock();
+	printf("Starting Time = %ld \n", startClock);
+
 	// Create threads
+	printf("Creating Threads\n");
 	pthread_t tid[THREAD_COUNT];
-	for (int i = 0; i < THREAD_COUNT; i++) 
-		pthread_create(&tid[i], NULL, mygemm, (void*)&tid);
-       
+	for (int i = 0; i < THREAD_COUNT; i++)
+		pthread_create(&tid[i], NULL, mygemm, (void*)& tid);
+
 	// Join threads - wait for specified thread to terminate
-    for (int j = 0; j < THREAD_COUNT; j++) 
+	printf("Terminating Threads\n");
+	for (int j = 0; j < THREAD_COUNT; j++)
 		pthread_join(tid[j], NULL);
-		
-	// Terminate if any more threads are running
-	pthread_exit(NULL);
-	
+
+
 	// End Time
-	gettimeofday(&stop, NULL);
-	
-	// Print Matrix if needed.
-	print_Matrix(matC);
-	
+	endClock = clock();
+	printf("Ending Time = %ld \n", endClock);
+
+	// Print Matrix if needed. The output will be very long so you'll lose any other debug printouts. Just leave it commented out unless you need it.
+	//print_Matrix(matC);
+
 	// Execution time formula for milliseconds
-	printf("Time of execution in milliseconds: %lu\n", (stop.tv_sec - start.tv_sec) * 1000 / CLOCKS_PER_SEC);
+	printf("Total Execution Time = %f secs \n", ((double) (endClock - startClock) / CLOCKS_PER_SEC));
 	return 0;
 }
